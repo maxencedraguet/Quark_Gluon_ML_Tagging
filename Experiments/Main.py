aa91e52e8947dec6361497204dddeb6d5f062a65
Maxence Draguet <maxdraguet@hotmail.com>
2020-06-04 20:03:06 +0200

    Updated Main and base_config to include the new Neural Network model and runner

--- Experiments/Main.py
+++ Experiments/Main.py
@@ -17,10 +17,6 @@ import datetime
 import os
 import yaml
 
-import random
-import numpy as np
-
-
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-config', type=str, help='path to configuration file', default='base_config.yaml')
@@ -30,7 +26,7 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
     main_file_path = os.path.dirname(os.path.realpath(__file__))
-    
+    print(main_file_path)
     # Read and store base configuration parameters
     base_config_full_path = os.path.join(main_file_path, args.config)
     with open(base_config_full_path, 'r') as yaml_file:
@@ -49,10 +45,14 @@ if __name__ == "__main__":
     experiment_parameters.set_property("experiment_timestamp", exp_timestamp)
     experiment_parameters.set_property("df_log_path", os.path.join(log_path, 'data_logger.csv'))
 
-    # Set a seed value for reproducibility.
+    # Set a seed value to packages with non-deterministic initialisaiton for reproducibility
+    import random
+    import numpy as np
+    import torch
     seed_value = experiment_parameters.get("seed")
     random.seed(seed_value)
     np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
     experiment_parameters._config["seed"] = seed_value
 
     # write copy of config_yaml in model_checkpoint_folder (also creating Results folder if necessary)
@@ -60,5 +60,7 @@ if __name__ == "__main__":
 
     if experiment_parameters.get(["experiment_type"]) == "BDT":
         runner = Models.BDTRunner(config=experiment_parameters)
+    elif experiment_parameters.get(["experiment_type"]) == "NN":
+        runner = Models.NNRunner(config=experiment_parameters)
     elif experiment_parameters.get(["experiment_type"]) == "UpRootTransformer":
         runner = DataLoaders.UpRootTransformer(config=experiment_parameters)
